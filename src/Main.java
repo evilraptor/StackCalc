0a42b4ecdf88ef26ceb92d9911dd92b79e2fd5d9
evilraptor <nikulin5.80mail@mail.ru>
2023-02-10 06:46:04 +0700

    SimplePotato4

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.util.Stack;
 
 public final class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Its me MARIO");
+        //System.out.println("Hello world! Its me MARIO");
         StackCalculator calcStack = new StackCalculator();
-        calcStack.a();
+        //calcStack.DEFINE("a", 5);
+        //calcStack.PUSH("a");
+        //calcStack.PUSH("2");
+        //calcStack.PRINT();
     }
 }
--- src/StackCalculator.java
+++ src/StackCalculator.java
@@ -1,30 +1,23 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
 public class StackCalculator {
-    Stack<Float> numbersStack;
+    Stack<Float> numbersStack = new Stack<>();
 
-    public final void a() {
-        System.out.println("7334");
-    }
+    void doCalculation(String input) {
+        ArithmeticOperations arithmetic = new ArithmeticOperations();
+        if (input == "+") arithmetic.ADD(numbersStack);
+        if (input == "-") arithmetic.SUB(numbersStack);
+        if (input == "*") arithmetic.MUL(numbersStack);
+        if (input == "/") arithmetic.DIV(numbersStack);
+        if (input == "SQRT") arithmetic.SQRT(numbersStack);
 
-    public void PUSH(float inputNumber) {
-        numbersStack.push(inputNumber);
-    }
-
-    public float POP(float inputNumber) {
-        float outputNumber = numbersStack.pop();
-        return outputNumber;
-    }
-
-    public float PRINT() {
-        float outputNumber = numbersStack.peek();
-        return outputNumber;
-    }
-
-    Map<String, Float> parameters;
-
-    public void DEFINE(String inputParameter, float value) {
-        parameters.put(inputParameter, value);
+        Map<String, Float> parameters = new HashMap<>();
+        CalcOperations calcOperations = new CalcOperations();
+        if (input == "DEFINE") calcOperations.DEFINE(numbersStack, parameters, input);
+        if (input == "PUSH") calcOperations.PUSH(numbersStack, parameters, input);
+        if (input == "POP") calcOperations.POP(numbersStack, parameters);
+        if (input == "PRINT") calcOperations.PRINT(numbersStack, parameters);
     }
 }
