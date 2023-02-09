13ea27504309728a3afe8310100778915bb14e0a
evilraptor <nikulin5.80mail@mail.ru>
2023-02-09 17:24:46 +0700

    SimplePotato3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Stack;
+
 public final class Main {
     //SimplePotato1
     public static void main(String[] args) {
         System.out.println("Hello world! Its me MARIO");
-
-        CalcCommands calcStack = new CalcCommands();
+        StackCalculator calcStack = new StackCalculator();
         calcStack.a();
     }
 }
