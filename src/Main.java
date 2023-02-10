ce6244666ff64c54b7792c6638d9076ee413ec62
evilraptor <nikulin5.80mail@mail.ru>
2023-02-10 13:56:39 +0700

    SimplePotato6

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public final class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world! Its me MARIO");
         StackCalculator calcStack = new StackCalculator();
-        calcStack.doCalculation("DEFINE a 4");
+        Scanner reader = new Scanner(System.in);
+        String input = reader.toString();
+
+        reader.close();
+        /*calcStack.doCalculation("DEFINE a 4");
         calcStack.doCalculation("PUSH a");
         calcStack.doCalculation("SQRT");
         //calcStack.doCalculation("PUSH 5");
@@ -13,6 +18,6 @@ public final class Main {
         //calcStack.DEFINE("a", 5);
         //calcStack.PUSH("a");
         //calcStack.PUSH("2");
-        //calcStack.PRINT();
+        //calcStack.PRINT();*/
     }
 }
