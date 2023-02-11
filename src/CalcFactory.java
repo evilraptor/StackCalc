7aff88bd1a1076720cdd14511da4040459be321f
evilraptor <nikulin5.80mail@mail.ru>
2023-02-11 21:14:14 +0700

    SimplePotato11

--- src/CalcFactory.java
+++ src/CalcFactory.java
@@ -1,5 +1,57 @@
-public interface CalcFactory {
-    ICalcCommands createCalcCommands();
+public class CalcFactory implements AbstractCalcFactory {
+    CalcFactory() {
+        ICalcCommand define = new DEFINE();
+        addCalcCommands("DEFINE", define);
+        ICalcCommand print = new PRINT();
+        addCalcCommands("PRINT", print);
+        ICalcCommand pop = new POP();
+        addCalcCommands("POP", pop);
+        ICalcCommand push = new PUSH();
+        addCalcCommands("PUSH", push);
 
-    IArithmeticOperations createArithmeticOperation();
+        IArithmeticOperation add = new ADD();
+        addArithmeticOperations("+", add);
+        IArithmeticOperation sub = new SUB();
+        addArithmeticOperations("-", sub);
+        IArithmeticOperation mul = new MUL();
+        addArithmeticOperations("*", mul);
+        IArithmeticOperation div = new DIV();
+        addArithmeticOperations("/", div);
+        IArithmeticOperation sqrt = new SQRT();
+        addArithmeticOperations("SQRT", sqrt);
+    }
+
+    @Override
+    public void addCalcCommands(String commandName, ICalcCommand inputCalcCommand) {
+        calcCommands.put(commandName, inputCalcCommand);
+    }
+
+    @Override
+    public void addArithmeticOperations(String operationName, IArithmeticOperation inputArithmeticOperation) {
+        arithmeticOperations.put(operationName, inputArithmeticOperation);
+    }
+
+    @Override
+    public void doCalcCommand(ListForCalculatorArguments listForCalculatorArguments) {
+        String[] inputArray = listForCalculatorArguments.getText().split(" ");
+        ICalcCommand outputCalcCommand = calcCommands.get(inputArray[0]);
+        outputCalcCommand.getResult(listForCalculatorArguments);
+        //new <ICalcCommand>;
+    }
+
+    @Override
+    public void doArithmeticOperation(ListForCalculatorArguments listForCalculatorArguments) {
+        //IArithmeticOperation a = arithmeticOperations.get("+");
+        String[] inputArray = listForCalculatorArguments.getText().split(" ");
+        IArithmeticOperation outputArithmeticOperation = arithmeticOperations.get(inputArray[0]);
+        outputArithmeticOperation.getResult(listForCalculatorArguments);
+    }
+
+    @Override
+    public void doCalc(ListForCalculatorArguments listForCalculatorArguments) {
+        String[] inputArray = listForCalculatorArguments.getText().split(" ");
+        if (calcCommands.containsKey(inputArray[0])) doCalcCommand(listForCalculatorArguments);
+        else if (arithmeticOperations.containsKey(inputArray[0])) doArithmeticOperation(listForCalculatorArguments);
+        else System.out.println("There is no command like this...");
+    }
 }
