3c9507794fe5f936173f31ffc46350599cee0a80
evilraptor <nikulin5.80mail@mail.ru>
2023-02-22 23:45:05 +0700

    SimplePotato13

--- src/Major/Main.java
+++ src/Major/Main.java
@@ -6,16 +6,19 @@ public final class Main {
     public static void main(String[] args) {
         //System.out.println(args[0]);
         StackCalculator stackCalculator = new StackCalculator();
-
-        Scanner in = new Scanner(System.in);
-        String input = "1";
-        int counter = 0;
-        while (!input.equals("EXIT")) {
-            System.out.print(counter + ":   ");
-            input = in.nextLine();
-            stackCalculator.doCalculation(input);
-            counter++;
+        if (args.length == 0) {
+            System.out.println("(no args in the input)");
+        } else {
+            Scanner in = new Scanner(System.in);
+            String input = "1";
+            int counter = 0;
+            while (!input.equals("EXIT")) {
+                System.out.print(counter + ":   ");
+                input = in.nextLine();
+                stackCalculator.doCalculation(input);
+                counter++;
+            }
+            in.close();
         }
-        in.close();
     }
 }
